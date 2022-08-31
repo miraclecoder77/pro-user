3060fb072b8873aacbd980c5282c4f997ed0bdbf
Miracle Aghamobi <miracle.aghamobi@globalaccelerex.com>
2022-08-31 12:05:08 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { HeroPageComponent } from './hero-page/hero-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HeroPageComponent },
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'log-in', loadChildren: () => import('./log-in/log-in.module').then(m => m.LogInModule) },
+  { path: 'sign-up', loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDVGPu7SWSAba4pOlABZh1AgvRaJGf2-g4",
+    authDomain: "ziblo-f0f34.firebaseapp.com",
+    projectId: "ziblo-f0f34",
+    storageBucket: "ziblo-f0f34.appspot.com",
+    messagingSenderId: "597169558159",
+    appId: "1:597169558159:web:8988ad79b95ec6db0756fa",
+    measurementId: "G-HESL8N4V7P"
+  },
 };
 
 /*
